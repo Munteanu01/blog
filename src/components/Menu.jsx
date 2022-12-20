803cae0802f7b448f03b2b8f986541874a15e9a5
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 12:45:48 +0200

    Menu before styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useState } from 'react';
 export default function Menu() {
-    return(
-        <nav>
-            <a>Menu</a>
-        </nav>
-    )
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    }
+    return (
+    <>
+        <button onClick={toggleMenu} className="md:hidden">
+            Toggle Menu
+        </button>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:block`}>
+            <ul>
+            <li><a href="">Menu item 1</a></li>
+            <li><a href="">Menu item 2</a></li>
+            <li><a href="">Menu item 3</a></li>
+            </ul>
+        </div>
+    </>
+  );
 }
