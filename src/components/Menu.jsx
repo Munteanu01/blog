ffb86ec45bbe0e0cae9236d9d89698a94106cd37
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 17:38:01 +0200

    repairing

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,20 +7,20 @@ export default function Menu() {
     setIsOpen(!isOpen);
     }
     return (
-    <nav className="md:flex block justify-between">
-        <div className="md:inline-block flex">
-        <a href=""><img className="w-[48px] ml-5" src="./img/logo.png"></img></a>
-        <button className="md:hidden flex ml-auto mr-3" onClick={toggleMenu}>
+    <nav className="">
+        <div className="">
+        <a href=""><img className="w-[48px]" src="./img/logo.png"></img></a>
+        <button className="" onClick={toggleMenu}>
             <Squeeze size={30} duration={0.2} color="black" label="Show menu"/>
         </button>
         </div>
-        <div className={`${isOpen ? 'block h-[100vh] md:h-[auto]' : 'hidden'} md:flex`}>
-            <ul className="md:flex text-center md:py-[13px] py-[25vh] ">
-            <li><a className="px-5 md:leading-[1rem] leading-[4rem]" href="">Menu 1</a></li>
-            <li><a className="px-5 md:leading-[1rem] leading-[4rem]" href="">Menu 2</a></li>
-            <li><a className="px-5 md:leading-[1rem] leading-[4rem]" href="">Menu 3</a></li>
+        <div className="">
+            <ul className="">
+            <li><a className="" href="">Menu 1</a></li>
+            <li><a className="" href="">Menu 2</a></li>
+            <li><a className="" href="">Menu 3</a></li>
             </ul>
         </div>
     </nav>
-  );
+    );
 }
