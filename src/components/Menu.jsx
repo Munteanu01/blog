4a548b5835481b4adedddf36365258613ae283e2
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 22:44:27 +0200

    Adding nav opacity

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,8 +14,13 @@ export default function Menu() {
         <button className="z-[2] md:hidden" onClick={toggleMenu}>
             <Squeeze size={30} duration={0.3} color="black" label="Show menu"/>
         </button>
-        <div className={`md:py-0 py-10 absolute md:static bg-white md:z-auto z-[1] w-full md:w-auto transition-all duration-[400ms] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-100px]'}`}>
-            <ul className="flex justify-between mx-20">
+        <div className={`md:py-0 py-10 absolute md:static bg-white md:z-auto z-[1] w-full md:w-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
+            <ul className="flex justify-between mx-20"
+                style={{
+                    animationName: isOpen ? 'navOpacityOn' : 'navOpacityOff',
+                    animationDuration: isOpen ? '300ms' : '150ms',
+                    animationFillMode: 'both',
+                }}>
             <li><a className="" href="">Menu 1</a></li>
             <li><a className="px-10" href="">Menu 2</a></li>
             <li><a className="" href="">Menu 3</a></li>
