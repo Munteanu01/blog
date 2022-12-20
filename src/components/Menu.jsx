fe659c6c77b96df876f326ce8066875038c8f73c
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 13:41:54 +0200

    Adding hamburger menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { useState } from 'react';
+import { Squeeze as Hamburger, Squeeze } from 'hamburger-react'
 export default function Menu() {
     const [isOpen, setIsOpen] = useState(false);
     const toggleMenu = () => {
     setIsOpen(!isOpen);
     }
     return (
-    <nav className="my-4">
-        <button onClick={toggleMenu} className="md:hidden">
-            Toggle Menu
+    <nav className="">
+        <button className="md:hidden flex ml-auto mr-1" onClick={toggleMenu}>
+            <Squeeze size={30} duration={0.2} color="black" label="Show menu"/>
         </button>
         <div className={`${isOpen ? 'block h-[100vh] md:h-[auto]' : 'hidden'} md:block `}>
             <ul className="md:flex">
-            <li><a href="">Menu item 1</a></li>
-            <li><a href="">Menu item 2</a></li>
-            <li><a href="">Menu item 3</a></li>
+            <li><a className="px-5" href="">Menu item 1</a></li>
+            <li><a className="px-5" href="">Menu item 2</a></li>
+            <li><a className="px-5" href="">Menu item 3</a></li>
             </ul>
         </div>
     </nav>
