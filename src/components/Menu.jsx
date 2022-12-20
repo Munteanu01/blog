b257a3112f91ae87ad8f84629c05b85996739e02
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 15:02:04 +0200

    q

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,14 +7,14 @@ export default function Menu() {
     setIsOpen(!isOpen);
     }
     return (
-    <nav>
+    <nav className="md:flex block">
         <div className="md:inline-block flex">
         <a href=""><img className="w-[48px] ml-5" src="./img/logo.png"></img></a>
         <button className="md:hidden flex ml-auto mr-3" onClick={toggleMenu}>
             <Squeeze size={30} duration={0.2} color="black" label="Show menu"/>
         </button>
         </div>
-        <div className={`${isOpen ? 'block h-[100vh] md:h-[auto]' : 'hidden'} md:inline-block `}>
+        <div className={`${isOpen ? 'block h-[100vh] md:h-[auto]' : 'hidden'} md:flex`}>
             <ul className="md:flex">
             <li><a className="px-5" href="">Menu 1</a></li>
             <li><a className="px-5" href="">Menu 2</a></li>
