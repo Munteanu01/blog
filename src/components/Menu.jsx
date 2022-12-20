099304c5d068bc9ef6a86cbdf4100085a4aa2dc4
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 18:28:41 +0200

    fixing navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,17 +7,17 @@ export default function Menu() {
     setIsOpen(!isOpen);
     }
     return (
-    <nav className="">
-        <div className="">
-        <a href=""><img className="w-[48px]" src="./img/logo.png"></img></a>
-        <button className="" onClick={toggleMenu}>
-            <Squeeze size={30} duration={0.2} color="black" label="Show menu"/>
+    <nav className="flex justify-between top-0">
+        <a className="z-[2]" href="">
+            <img className="w-[48px] ml-5" src="./img/logo.png"></img>
+        </a>
+        <button className="z-[2] md:hidden" onClick={toggleMenu}>
+            <Squeeze size={30} duration={0.3} color="black" label="Show menu"/>
         </button>
-        </div>
-        <div className="">
-            <ul className="">
+        <div className={`md:py-0 py-10 absolute md:static bg-white md:z-auto z-[1] w-full md:w-auto transition-all duration-[400ms] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-100px]'}`}>
+            <ul className="flex justify-between mx-20">
             <li><a className="" href="">Menu 1</a></li>
-            <li><a className="" href="">Menu 2</a></li>
+            <li><a className="px-10" href="">Menu 2</a></li>
             <li><a className="" href="">Menu 3</a></li>
             </ul>
         </div>
