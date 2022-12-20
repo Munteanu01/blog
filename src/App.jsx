767cb7184c9fb8de8f17e423efaf229d13e055b1
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 12:34:20 +0200

    Creating menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Home from "./components/Home"
-
+import Menu from "./components/Menu"
 import { GraphQLClient, gql } from 'graphql-request';
 const hygraph = new GraphQLClient('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbtaryeo1x2o01uf7y9oe08j/master');
 const QUERY = gql`
@@ -21,6 +21,7 @@ const { posts } = await hygraph.request(QUERY)
 export default function App() {
   return (
     <>
+    <Menu />
     <Home posts={posts}/>
     </>
   )
