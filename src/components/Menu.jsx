5f9e609318601cea43f63f6a567348de977c91c2
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-20 14:46:52 +0200

    adding logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,15 +7,16 @@ export default function Menu() {
     setIsOpen(!isOpen);
     }
     return (
-    <nav className="">
-        <button className="md:hidden flex ml-auto mr-1" onClick={toggleMenu}>
+    <nav className={`${isOpen ? 'block' : 'flex'}`}>
+        <a href=""><img className={`${isOpen ? 'hidden' : 'w-[48px] ml-5'}`} src="./img/logo.png"></img></a>
+        <button className="md:hidden flex ml-auto mr-3" onClick={toggleMenu}>
             <Squeeze size={30} duration={0.2} color="black" label="Show menu"/>
         </button>
         <div className={`${isOpen ? 'block h-[100vh] md:h-[auto]' : 'hidden'} md:block `}>
             <ul className="md:flex">
-            <li><a className="px-5" href="">Menu item 1</a></li>
-            <li><a className="px-5" href="">Menu item 2</a></li>
-            <li><a className="px-5" href="">Menu item 3</a></li>
+            <li><a className="px-5" href="">Menu 1</a></li>
+            <li><a className="px-5" href="">Menu 2</a></li>
+            <li><a className="px-5" href="">Menu 3</a></li>
             </ul>
         </div>
     </nav>
