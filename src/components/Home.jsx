e0e7758c3bc51085c36b7b305dc4324d3e5f9741
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-22 10:47:33 +0200

    Navbar big screens starting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,15 +3,15 @@ import DOMPurify from "dompurify";
 export default function Home(props) {
     const posts = props.posts
     return(
-        <>
+        <div className='mt-40 grid justify-between mx-40'>
             {posts.map((post) => (
-            <div className="break-words max-w-full" key={post.id}>
+            <div className="break-words max-w-md" key={post.id}>
                 <img src={post.photo.url}></img>
                 <h1 className='text-4xl'>{post.title}</h1>
                 <p>{post.date}</p>
                 <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content.html)}}></div>
             </div>
             ))}
-        </>
+        </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,20 +9,20 @@ export default function Menu() {
     setIsOpen(!isOpen);
     }
     return (
-    <nav className="flex bg-white justify-between sticky top-0">
+    <nav className="flex bg-white justify-between md:justify-end md:pt-1 sticky top-0">
         <Link to="/Home" className="z-[2]"><img className="w-[48px] ml-5" src="./img/logo.png"></img></Link> 
         <button className="z-[2] md:hidden mr-5" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu.png'alt=""/></button>
-        <div className={`md:py-0 sm:py-7 absolute md:static bg-white md:z-auto z-[1] w-full md:w-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
+        <div className={`md:py-0 md:flex md:items-center sm:py-7 absolute md:static bg-white md:z-auto z-[1] w-full md:w-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
             <ul className="sm:flex grid grid-cols-2 justify-between mx-24 font-semibold text-lg"
                 style={{
                     animationName: isOpen ? 'navOpacityOn' : 'navOpacityOff',
                     animationDuration: isOpen ? '300ms' : '150ms',
                     animationFillMode: 'both',
                 }}>
-            <li className="col-span-1 text-center pb-1" onClick={toggleMenu}><Link to="/Home">Home</Link></li>
-            <li className="col-span-1 text-center pb-1" onClick={toggleMenu}><Link to="/Tech">Tech</Link></li>
-            <li className="col-span-1 text-center pb-2" onClick={toggleMenu}><Link to="/Science">Science</Link></li>
-            <li className="col-span-1 text-center pb-2" onClick={toggleMenu}><Link to="/All">All</Link></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Home">Home</Link></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Tech">Tech</Link></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/Science">Science</Link></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/All">All</Link></li>
             </ul>
         </div>
     </nav>
