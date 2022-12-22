7e8abf279ed3d3cf0c48c5c0976cb4d6331a101c
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-22 19:43:36 +0200

    Responsive home posts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,16 @@ import DOMPurify from "dompurify";
 export default function Home(props) {
     const posts = props.posts
     return(
-        <div className='mt-40 justify-between md:mx-40'>
+        <div className='md:mx-32 mx-12 mt-20'>
+
+            <div className='sm:grid gap-5 grid-cols-2 lg:grid-cols-3 lg:gap-3'>
             {posts.map((post) => (
-            <div className="break-words max-w-md" key={post.id}>
+            <div className="break-words w-full max-w-xs lg:max-w-sm mt-10 mx-auto border-2 border-white" key={post.id}>
                 <div className='object-cover'><img className=' w-full' src={post.photo.url}></img></div>
-                <h1 className='text-4xl'>{post.title}</h1>
-                <p>{post.date}</p>
+                <h1 className='text-xl'>{post.title}</h1>
             </div>
             ))}
+            </div>
         </div>
     )
 }
