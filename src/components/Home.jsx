4adec99fcf77b9ab529018a398d8f6c78404c548
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-22 17:41:28 +0200

    W

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,9 @@ export default function Home(props) {
         <div className='mx-12'>
         <div className='mt-20 sm:grid sm:grid-cols-2 lg:grid-cols-3 justify-items-center'>
             {posts.map((post) => (
-            <div className="break-words mx-auto mt-5 border-2 sm:mx-2" key={post.id}>
-                <div>
-                    <img src={post.photo.url} alt="" />
+            <div className="break-words mx-auto w-[250px] mt-5 border-2 sm:mx-2" key={post.id}>
+                <div className='aspect-w-16  aspect-h-9'>
+                    <img className='w-full object-cover' src={post.photo.url} alt="" />
                 </div>
                 <div className='flex justify-between'>
                 <h1 className=''>{post.title}</h1>
