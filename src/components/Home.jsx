a48964bb2c1e1ffa59f7c9e80c39596b26ff584b
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-22 17:14:50 +0200

    starting to fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ export default function Home(props) {
         <div className='mx-12'>
         <div className='mt-20 sm:grid sm:grid-cols-2 lg:grid-cols-3 justify-items-center'>
             {posts.map((post) => (
-            <div className="break-words max-w-xs mx-auto mt-5 border-2 sm:mx-2" key={post.id}>
-                <img src={post.photo.url}></img>
+            <div className="break-words w-3/4 px-5 h-60 mx-auto mt-5 border-2 sm:mx-2" key={post.id}>
+                <div class="bg-no-repeat bg-center bg-cover bg-white h-full w-full" style={{ backgroundImage: `url(${post.photo.url})` }}></div>
                 <div className='flex justify-between'>
                 <h1 className=''>{post.title}</h1>
                 <p>{post.date}</p>
