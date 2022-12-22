fe6c2884c3ac7db3c0b2c71cfa6a51ac67a9969c
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-22 18:04:44 +0200

    q

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,15 @@ import DOMPurify from "dompurify";
 export default function Home(props) {
     const posts = props.posts
     return(
-        <div className='mx-12'>
-        <div className='mt-20 sm:grid sm:grid-cols-2 lg:grid-cols-3 justify-items-center'>
+        <div className='mt-40 justify-between md:mx-40'>
             {posts.map((post) => (
-            <div className="break-words mx-auto w-[250px] mt-5 border-2 sm:mx-2" key={post.id}>
-                <div className='aspect-w-16  aspect-h-9'>
-                    <img className='w-full object-cover' src={post.photo.url} alt="" />
-                </div>
-                <div className='flex justify-between'>
-                <h1 className=''>{post.title}</h1>
+            <div className="break-words max-w-md" key={post.id}>
+                <img src={post.photo.url}></img>
+                <h1 className='text-4xl'>{post.title}</h1>
                 <p>{post.date}</p>
-                </div>
+                <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content.html)}}></div>
             </div>
             ))}
         </div>
-        </div>
     )
 }
-// <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content.html)}}></div>
