b1882f9c0baa6a5f9e95f6f7c1f3c4a1da67750f
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-23 17:00:41 +0200

    Home most recent posts

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -1,6 +1,19 @@
-import React from "react";
-export default function All() {
+import React from 'react'
+import { Link } from 'react-router-dom';
+export default function Home(props) {
+    const posts = props.posts
     return(
-    <h1>All</h1>
+        <div className='md:mx-32 mx-12 mt-20'>
+            <div className='sm:grid gap-5 grid-cols-2 lg:grid-cols-3 lg:gap-3'>
+            {posts.map((post) => (
+            <div className="break-words w-full max-w-xs lg:max-w-sm mt-10 mx-auto border-2 border-white" key={post.id}>
+                <Link to={`/posts/${post.slug}`}>
+                <div className='object-cover border-b-2 border-white'><img className=' w-full' src={post.photo.url}></img></div>
+                <h1 className='text-2xl pt-2 text-center'>{post.title}</h1>
+                </Link>
+            </div>
+            ))}
+            </div>
+        </div>
     )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import DOMPurify from "dompurify";
 export default function Post(props) {
     const {slug} = useParams()
     const posts = props.posts
@@ -8,6 +9,7 @@ export default function Post(props) {
         <div className="max-w-lg mx-auto pt-20">
         <h1>This is {post.title}</h1>
         <img src={post.photo.url} alt="" />
+        <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content.html)}}></div>
         </div>
     )
 }
