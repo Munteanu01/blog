a0716b3a3536fc6b3595efbe2a754c837c149f53
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-23 17:33:31 +0200

    adding most recent post home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 export default function Home(props) {
     const posts = props.posts
-    const mostRecentPosts = posts.slice().sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 7);
+    const mostRecentPost = posts.sort((a, b) => new Date(b.date) - new Date(a.date))[0];
+    const mostRecentPosts = posts.slice().sort((a, b) => new Date(b.date) - new Date(a.date)).slice(1, 7);
     return(
         <div className='md:mx-32 mx-12 mt-20'>
+            <div className='mx-auto max-w-lg'>
+                <Link to={`/posts/${mostRecentPost.slug}`}>
+                <h1 className='text-4xl'>{mostRecentPost.title}</h1>
+                <img className='w-full' src={mostRecentPost.photo.url} alt="" />
+                </Link>
+            </div>
             <div className='sm:grid gap-5 grid-cols-2 lg:grid-cols-3 lg:gap-3'>
             {mostRecentPosts.map((post) => (
             <div className="break-words w-full max-w-xs lg:max-w-sm mt-10 mx-auto border-2 border-white" key={post.id}>
