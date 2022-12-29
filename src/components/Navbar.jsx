6eeded21c89a30fe76c4c884a392038935e65a97
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-29 15:58:38 +0200

    Navbar done styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 export default function Menu() {
@@ -23,25 +23,22 @@ export default function Menu() {
     setIsOpen(!isOpen);
     }
     return (
-    <nav className={`dark:bg-black bg-white flex justify-between lg:justify-end lg:py-2 md:ml-20 lg:ml-0 sticky transition-all duration-[0.4s] ${visible ? "top-0" : "top-[-150px]"}`}>
-        <Link to="/Home" className="z-[2] hidden dark:block"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-light.png"></img></Link> 
-        <Link to="/Home" className="z-[2] block dark:hidden"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-dark.png"></img></Link> 
+    <nav className={`dark:bg-black bg-white flex justify-between lg:justify-end lg:py-2 md:ml-32 lg:ml-0 lg:mr-10 sticky transition-all duration-[0.4s] ${visible ? "top-0" : "top-[-150px]"}`}>
+        <NavLink to="/Home" className="z-[2] hidden dark:block"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-light.png"></img></NavLink> 
+        <NavLink to="/Home" className="z-[2] block dark:hidden"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-dark.png"></img></NavLink> 
         <button className="z-[2] md:hidden mr-5 hidden dark:block" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu-light.png'alt=""/></button>
         <button className="z-[2] md:hidden mr-5 block dark:hidden" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu-dark.png'alt=""/></button>
         <div className={`dark:bg-black bg-white md:py-0 md:flex md:items-center sm:py-7 absolute md:static md:z-auto z-[1] w-full md:w-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
-            <ul className="sm:flex grid grid-cols-2 justify-between mx-24 md:w-[40vw] lg:w-[33vw] md:max-w-[550px] md:mr-20 lg:mr-28 lg:ml-10 md:ml-10 font-semibold text-lg"
+            <ul className="sm:flex grid grid-cols-2 justify-between mx-24 md:w-[40vw] lg:w-[33vw] md:max-w-[550px] md:mr-36 lg:mr-28 lg:ml-10 md:ml-10 font-semibold text-lg"
                 style={{
                     animationName: isOpen ? 'navOpacityOn' : 'navOpacityOff',
                     animationDuration: isOpen ? '300ms' : '150ms',
                     animationFillMode: 'both',
                 }}>
-            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Home">Home</Link></li>
-            <span className="hidden md:inline-block">/</span>
-            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Tech">Tech</Link></li>
-            <span className="hidden md:inline-block">/</span>
-            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/Science">Science</Link></li>
-            <span className="hidden md:inline-block">/</span>
-            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/All">All</Link></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><NavLink to="/Home" activeClassName="active">Home <span className="hidden md:inline-block">/</span></NavLink></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><NavLink to="/Tech" activeClassName="active">Tech <span className="hidden md:inline-block">/</span></NavLink></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><NavLink to="/Science" activeClassName="active">Science <span className="hidden md:inline-block">/</span></NavLink></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><NavLink to="/All" activeClassName="active">All <span className="hidden md:inline-block">/</span></NavLink></li>
             </ul>
         </div>
     </nav>
