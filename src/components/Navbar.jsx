ef134c9a6fd99151b382bfea379d6c4d8fc12710
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-29 16:43:12 +0200

    adding fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 export default function Menu() {
@@ -24,8 +24,8 @@ export default function Menu() {
     }
     return (
     <nav className={`dark:bg-black bg-white flex justify-between lg:justify-end lg:py-2 md:ml-32 lg:ml-0 lg:mr-10 sticky transition-all duration-[0.4s] ${visible ? "top-0" : "top-[-150px]"}`}>
-        <NavLink to="/Home" className="z-[2] hidden dark:block"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-light.png"></img></NavLink> 
-        <NavLink to="/Home" className="z-[2] block dark:hidden"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-dark.png"></img></NavLink> 
+        <Link to="/Home" className="z-[2] hidden dark:block"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-light.png"></img></Link> 
+        <Link to="/Home" className="z-[2] block dark:hidden"><img className="w-[48px] md:w-[50px] ml-5" src="../img/logo-dark.png"></img></Link> 
         <button className="z-[2] md:hidden mr-5 hidden dark:block" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu-light.png'alt=""/></button>
         <button className="z-[2] md:hidden mr-5 block dark:hidden" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu-dark.png'alt=""/></button>
         <div className={`dark:bg-black bg-white md:py-0 md:flex md:items-center sm:py-7 absolute md:static md:z-auto z-[1] w-full md:w-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
@@ -35,10 +35,10 @@ export default function Menu() {
                     animationDuration: isOpen ? '300ms' : '150ms',
                     animationFillMode: 'both',
                 }}>
-            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><NavLink to="/Home" activeClassName="active">Home <span className="hidden md:inline-block">/</span></NavLink></li>
-            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><NavLink to="/Tech" activeClassName="active">Tech <span className="hidden md:inline-block">/</span></NavLink></li>
-            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><NavLink to="/Science" activeClassName="active">Science <span className="hidden md:inline-block">/</span></NavLink></li>
-            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><NavLink to="/All" activeClassName="active">All <span className="hidden md:inline-block">/</span></NavLink></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Home">Home <span className="hidden md:inline-block">/</span></Link></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Tech">Tech <span className="hidden md:inline-block">/</span></Link></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/Science">Science <span className="hidden md:inline-block">/</span></Link></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/All">All <span className="hidden md:inline-block">/</span></Link></li>
             </ul>
         </div>
     </nav>
