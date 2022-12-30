8aae4944d9390e977bd2c873d4b379fbe4f8d1f1
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2022-12-30 12:42:48 +0200

    nav editing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,18 +29,19 @@ export default function Menu() {
         <Link to="/Home" className="z-[2] block dark:hidden"><img className="w-[48px] ml-5 md:ml-0" src="../img/logo-dark.png"></img></Link> 
         <button className="z-[2] md:hidden mr-5 hidden dark:block" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu-light.png'alt=""/></button>
         <button className="z-[2] md:hidden mr-5 block dark:hidden" onClick={toggleMenu}><img className="w-[24px]" src='/img/menu-dark.png'alt=""/></button>
-        <div className={`dark:bg-black bg-white w-full md:w-auto md:static sm:py-7 md:flex md:py-0 md:items-center  
-            absolute z-[1] md:z-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
-            <ul className="sm:flex grid grid-cols-2 justify-between font-semibold text-lg"
+        <div className={`w-full md:w-auto  sm:py-7 md:flex md:py-0 md:items-center  
+            dark:bg-black bg-white md:static absolute z-[1] md:z-auto transition-all duration-[0.4s] ease-in-out ${isOpen ? 'top-[40px] ':'top-[-80px]'}`}>
+            <ul className="grid grid-cols-2 justify-between max-w-[200px] mx-auto sm:flex sm:max-w-md 
+                font-semibold text-lg"
                 style={{
                     animationName: isOpen ? 'navOpacityOn' : 'navOpacityOff',
                     animationDuration: isOpen ? '300ms' : '150ms',
                     animationFillMode: 'both',
                 }}>
-            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Home">Home <span className="hidden md:inline-block">/</span></Link></li>
-            <li className="col-span-1 text-center pb-1 md:pb-0" onClick={toggleMenu}><Link to="/Tech">Tech <span className="hidden md:inline-block">/</span></Link></li>
-            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/Science">Science <span className="hidden md:inline-block">/</span></Link></li>
-            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/All">All <span className="hidden md:inline-block">/</span></Link></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0 md:pr-5" onClick={toggleMenu}><Link to="/Home">Home</Link></li>
+            <li className="col-span-1 text-center pb-1 md:pb-0 md:pr-5" onClick={toggleMenu}><Link to="/Tech">Tech</Link></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0 md:pr-5" onClick={toggleMenu}><Link to="/Science">Science</Link></li>
+            <li className="col-span-1 text-center pb-2 md:pb-0" onClick={toggleMenu}><Link to="/All">All</Link></li>
             </ul>
         </div>
     </nav>
