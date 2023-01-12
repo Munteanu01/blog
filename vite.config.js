5e5ee39e1aac8b9774e01bee326866fe4d8cc2f3
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-01-12 15:33:23 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+module.exports = {
+  outDir: 'build'
+}
 // https://vitejs.dev/config/
 export default defineConfig({
+  
   plugins: [react()]
 })
