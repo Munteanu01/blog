8831a18a71a31420c2be3f2f36849e2c5f0595de
Munteanu01 <105317034+Munteanu01@users.noreply.github.com>
2023-01-13 10:05:27 +0200

    fixing the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/Home"
 import Navbar from "./components/Navbar"
@@ -7,42 +8,44 @@ import Author from "./components/Author"
 import Mind from "./components/Mind"
 import Human from "./components/Human"
 import Philosophy from "./components/Philosophy"
-
-
-
-
-
 import { GraphQLClient, gql } from 'graphql-request';
-const hygraph = new GraphQLClient('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbtaryeo1x2o01uf7y9oe08j/master');
-const QUERY = gql`
-{ 
-  posts {
-    id
-    slug
-    author{
-      name
-      slug
-      posts {
-        id
-      }
-    }
-    postType {
-      type
-    } 
-    photo{
-      url
-    }
-    title
-    description
-    date
-    content{
-      html
-    }
-  } 
-}`
-const { posts } = await hygraph.request(QUERY)
-const mostRecentPosts = posts.slice().sort((a, b) => new Date(b.date) - new Date(a.date))
+
 export default function App() {
+  const [mostRecentPosts, setMostRecentPosts] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const hygraph = new GraphQLClient('https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clbtaryeo1x2o01uf7y9oe08j/master');
+      const QUERY = gql`
+      { 
+        posts {
+          id
+          slug
+          author{
+            name
+            slug
+            posts {
+              id
+            }
+          }
+          postType {
+            type
+          } 
+          photo{
+            url
+          }
+          title
+          description
+          date
+          content{
+            html
+          }
+        } 
+      }`
+      const { posts } = await hygraph.request(QUERY)
+      setMostRecentPosts(posts.slice().sort((a, b) => new Date(b.date) - new Date(a.date)))
+    }
+    fetchData()
+  }, [])
   return (
     <>
       <Navbar />
